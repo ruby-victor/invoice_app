47fe8d38e2cf1c8d3a3d344eee534bbb02b4eea5
victor <victor>
2014-05-19 18:26:00 +0530

    modified for structure collapse

--- app/controllers/invoices_controller.rb
+++ app/controllers/invoices_controller.rb
@@ -1,77 +1,77 @@
 class InvoicesController < ApplicationController
-	
-  def index
-	@invoices = Invoice.all
-	respond_to do |format|
-		format.html {}
-	end
-	
-  end
 
-  def new
-	@invoice = Invoice.new
-	@auto_number= Invoice.next_number
-	5.times do
-		@invoice.line_items.build
-	end
-	respond_to do |format|
-		format.html {}
-	end
+def index
+  @invoices = Invoice.all
+  respond_to do |format|
+   format.html {}
   end
+end
 
-  def create
-	@invoice = Invoice.new(params[:invoice])
-	@auto_number = params[:invoice][:number]
-	respond_to do |format|
-		if @invoice.save
-			format.html { redirect_to :action=> "show",:id=>@invoice.id}
-	        else
-			if @invoice.line_items.blank?
-			5.times do
-				@invoice.line_items.build
-			end
-			end
-			format.html {render :new}
-		end	
-	end
+def new
+  @invoice = Invoice.new
+  @auto_number= Invoice.next_number
+  5.times do
+   @invoice.line_items.build
   end
+  respond_to do |format|
+   format.html {}
+  end
+end
 
-  def edit
-	@invoice = Invoice.find(params[:id])
-	if @invoice.line_items.blank?
-			5.times do
-				@invoice.line_items.build
-			end
-	end		
-	respond_to do |format|
-		format.html {}
-	end
+def create
+  @invoice = Invoice.new(params[:invoice])
+  @auto_number = params[:invoice][:number]
+  respond_to do |format|
+    if @invoice.save
+      format.html { redirect_to :action=> "show",:id=>@invoice.id}
+    else
+      if @invoice.line_items.blank?
+        5.times do
+          @invoice.line_items.build
+        end
+      end
+				  format.html {render :new}
+    end	
   end
+end
 
-  def update
-	@invoice = Invoice.find(params[:id])
-	respond_to do |format|
-		if @invoice.update_attributes(params[:invoice])
-			format.html { redirect_to :action=> "show",:id=>@invoice.id}
-	        else
-			format.html {render :edit}
-		end
-	end
+def edit
+  @invoice = Invoice.find(params[:id])
+  if @invoice.line_items.blank? 
+    5.times do
+      @invoice.line_items.build
+    end
+  end		
+  respond_to do |format|
+    format.html {}
   end
-	
-  def show
-	@invoice=Invoice.find(params[:id])
-	respond_to do |format|
-		format.html {}
-	end
+end
+
+def update
+  @invoice = Invoice.find(params[:id])
+  respond_to do |format|
+    if @invoice.update_attributes(params[:invoice])
+      format.html { redirect_to :action=> "show",:id=>@invoice.id}
+    else
+      format.html {render :edit}
+    end
   end
-  
-  def destroy
-	@invoice = Invoice.find(params[:id])
-	@invoice.delete
-	respond_to do |format|
-		format.html { redirect_to :action=> "index"}
-	end
+end
+
+def show
+  @invoice=Invoice.find(params[:id])
+  respond_to do |format|
+    format.html {}
   end
+end
+
+def destroy
+  @invoice = Invoice.find(params[:id])
+  @invoice.delete
+  respond_to do |format|
+    format.html { redirect_to :action=> "index"}
+  end
+end
+
 end
 
